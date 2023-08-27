3f0b2509f5551b7a1189addb136455b3e4c025fb
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-08-27 12:45:33 +0900

    35 サインイン機能の実装【Authサービスの実装】

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import {UserService} from "../user/user.service";
+import {JwtService} from "@nestjs/jwt";
+import {User} from "@prisma/client";
+import * as bcrypt from "bcrypt";
+import {SignInResponse} from "./dto/signIn.response";
+import {JwtPayload} from "./types/jwtPayload.type";
+import {use} from "passport";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.userService.getUser(email);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+    return null;
+  }
+
+  async signIn(user: User): Promise<SignInResponse> {
+    const payload: JwtPayload = {email: user.email, sub: user.id};
+    return {accessToken: this.jwtService.sign(payload), user}
+  }
+}
